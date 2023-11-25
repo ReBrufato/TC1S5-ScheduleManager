4188bbf08f5003772c784d6905e36f97abae1673
Leandro Dolensi <leandro.dolensi@aluno.ifsp.edu.br>
2023-11-25 11:19:10 -0300

    feat: add object HomePage

---
+++ Selenium-Tests/src/test/java/pages/HomePage.java
@@ -0,0 +1,17 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    private By listaContato = By.id("contacts");
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        if (!driver.getTitle().equals("Agenda de contatos")) {
+            throw new IllegalStateException("Esta não é a página Agenda de contatos" +
+                    " a págiana enviada é: " + driver.getCurrentUrl());
+        }
+    }
+}
